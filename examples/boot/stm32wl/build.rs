2e2f185aba004ebcc78186fe04c5028217cd4308
Ulf Lilleengen <lulf@redhat.com>
2022-04-21 11:15:19 +0200

    Bootloader example for stm32wl

--- examples/boot/stm32wl/build.rs
+++ examples/boot/stm32wl/build.rs
@@ -1,3 +1,13 @@
+//! This build script copies the `memory.x` file from the crate root into
+//! a directory where the linker can always find it at build time.
+//! For many projects this is optional, as the linker always searches the
+//! project root directory -- wherever `Cargo.toml` is. However, if you
+//! are using a workspace or have a more complicated build setup, this
+//! build script becomes required. Additionally, by requesting that
+//! Cargo re-run the build script whenever `memory.x` is changed,
+//! updating `memory.x` ensures a rebuild of the application with the
+//! new memory settings.
+
 use std::env;
 use std::fs::File;
 use std::io::Write;
@@ -21,7 +31,5 @@ fn main() {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    if env::var("CARGO_FEATURE_DEFMT").is_ok() {
-        println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-    }
+    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
